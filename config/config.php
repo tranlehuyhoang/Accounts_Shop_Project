986ef1450797827feba2ce3d695d1fc287cc128a
Huyh Huyhu <2509roblox@gmail.com>
2023-09-07 21:24:59 +0700

    18

--- config/config.php
+++ config/config.php
@@ -1,7 +1,16 @@
 <?php
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "clonesnew");
-define("DB_POST", "3309");
-define("HOST", "http://localhost/clonesnew/");
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+// define("DB_NAME", "clonesnew");
+// define("DB_POST", "3309");
+// define("HOST", "http://localhost/clonesnew/");
+?>
+<?php
+define("DB_HOST", "sql304.infinityfree.com");
+define("DB_USER", "if0_34985443");
+define("DB_PASS", "tEXqhGQTILN0at");
+define("DB_NAME", "if0_34985443_clonesnew");
+define("DB_POST", "3306");
+define("HOST", "https://clonesnew.rf.gd");
+?>
